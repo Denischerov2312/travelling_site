c2b9530699a86f3dc096c64efe5eba6ea787efb8
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-07-22 12:44:10 +0300

    Подправил кнопки  у тг бота

--- tg_bot.py
+++ tg_bot.py
@@ -25,9 +25,7 @@ TOWNS = [
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
-    markup = ReplyKeyboardMarkup(
-        one_time_keyboard=True,
-        input_field_placeholder='Select town:')
+    markup = ReplyKeyboardMarkup(input_field_placeholder='Select town:')
     for towns in list(chunked(TOWNS, 2)):
         button_1 = KeyboardButton(towns[0])
         button_2 = KeyboardButton(towns[1])
