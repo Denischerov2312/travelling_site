a071b71f77ebca084859c36f26d525f204783dd5
Tkaz <egor4ig95@mail.ru>
2024-07-26 20:11:46 +0300

    Create render_website.py

---
+++ render_website.py
@@ -0,0 +1,54 @@
+import os
+import json
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+
+
+def get_files_path(folder):
+    files = []
+    for file_name in os.listdir(folder):
+        if os.path.isfile(os.path.join(folder, file_name)):
+            files.append(os.path.join(folder, file_name))
+    return files
+
+
+def get_file_content(file_name):
+    with open(file_name, 'r', encoding='utf8') as my_file:
+        files_content = json.load(my_file)
+    return files_content
+
+
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+
+    city_weather = get_file_content('weather/weather.json')
+
+    cities = get_files_path('cities')
+    city_index = cities.index(f'cities\\{city_weather['city']}.json')
+    city_excursions = get_file_content(cities[city_index])
+
+    rendered_page = template.render(
+        excursions=city_excursions,
+        weather=city_weather
+    )
+    with open('pages/index.html', 'w', encoding='utf8') as file:
+        file.write(rendered_page)
+
+
+def main():
+    Path('pages').mkdir(parents=True, exist_ok=True)
+    on_reload()
+
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.',  default_filename='pages/index.html')
+
+
+if __name__ == '__main__':
+    main()
