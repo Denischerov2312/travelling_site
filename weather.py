6379117e72b76e05c880dfcd217815def567fb96
Tkaz <egor4ig95@mail.ru>
2024-07-26 20:13:11 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,12 +3,12 @@ import json
 from pathlib import Path
 
 
-def get_weather(sity):
+def get_weather(city):
     Path('weather').mkdir(parents=True, exist_ok=True)
     url = 'http://api.weatherapi.com/v1/current.json'
     payload = {
         'key': 'e11a050020a94a0da64171440241907',
-        'q': sity,
+        'q': city,
         'lang': 'ru',
     }
     response = requests.get(url=url, params=payload)
@@ -21,6 +21,7 @@ def get_weather(sity):
         wind_dir = wind_dir.replace(eng_letter, ru_letter)
 
     weather = {
+        'city': city,
         'temp': response.json()['current']['temp_c'],
         'condition': response.json()['current']['condition']['text'],
         'humidity': response.json()['current']['humidity'],
@@ -28,6 +29,6 @@ def get_weather(sity):
         'wind_dir': wind_dir
         }
 
-    with open(f"weather/{sity}", "w", encoding='utf8') as file:
+    with open("weather/weather.json", "w", encoding='utf8') as file:
         json.dump(weather, file, ensure_ascii=False)
     return weather
