26b4fb2e55371a59275d403805c598578d21e47e
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-07-27 19:09:12 +0300

    Донастроил кнопку показать ещё

--- scripts/show_more.js
+++ scripts/show_more.js
@@ -1,6 +1,6 @@
 const showMore = document.querySelector('.button__show-more');
 const tourLength =  document.querySelectorAll('.tour').length;
-let items = 10;
+let items = 15;
 
 
 function check_button() {
@@ -12,7 +12,7 @@ function check_button() {
 check_button()
 
 showMore.addEventListener('click', () => {
-    items += 5
+    items += 10
     const array = Array.from(document.querySelector('.main__tours').children);
     const visItems = array.slice(0, items)
 
